1d1df377703c23f4eca933fa4ce23cc4125b12c5
vasilileliuh <vasili.leliuh@gmail.com>
2019-11-19 00:26:45 +0200

    added additional check for PIN range(1000-9999)exercise PinCodeApp

--- HW_vanar_18.11/Exercise1/src/PinCodeApp.java
+++ HW_vanar_18.11/Exercise1/src/PinCodeApp.java
@@ -7,20 +7,25 @@ public class PinCodeApp {
     public static void main(String[] args) throws IOException {
 
         final short CORRECT_PIN = 1234;
+        final short CORRECT_PIN_MIN = 1000;
+        final short CORRECT_PIN_MAX = 9999;
+
         short pin = 0;
         byte tries = 0;
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Please enter PIN code: ");
-        while (pin != CORRECT_PIN) {
-            if (tries == 3) {
-                System.out.println("TOO MANY TRIES!");
-                break;
+        if (CORRECT_PIN >= CORRECT_PIN_MIN && CORRECT_PIN <= CORRECT_PIN_MAX) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Please enter PIN code: ");
+            while (pin != CORRECT_PIN) {
+                if (tries == 3) {
+                    System.out.println("TOO MANY TRIES!");
+                    break;
+                }
+                pin = Short.parseShort(reader.readLine());
+                tries++;
             }
-            pin = Short.parseShort(reader.readLine());
-            tries++;
+            if (pin == CORRECT_PIN)
+                System.out.println("ACCESS GRANTED!");
         }
-        if (pin == CORRECT_PIN)
-            System.out.println("ACCESS GRANTED!");
     }
 }
