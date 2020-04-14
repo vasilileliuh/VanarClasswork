79ff13e9cfda949daf947fb21bc60fc683f4c0f3
vasilileliuh <vasili.leliuh@gmail.com>
2020-04-14 21:09:42 +0300

    added exercise with javaFX GUI

---
+++ HW_vanar_14.04/javafx/src/main/java/main/HelloMyFX.java
@@ -0,0 +1,28 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class HelloMyFX extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("/main.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
+
