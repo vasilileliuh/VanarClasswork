83a35b6cf766cae8e837e8b26c01a4a93e8279fd
vasilileliuh <vasili.leliuh@gmail.com>
2020-04-28 01:36:48 +0300

    project neo-mvn-cli-gui added Label error field into first.fxml

--- HW_vanar_04.04/neo-mvn-cli-gui/src/main/java/main/MainController.java
+++ HW_vanar_04.04/neo-mvn-cli-gui/src/main/java/main/MainController.java
@@ -3,19 +3,12 @@ package main;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.Stage;
 import space.Asteroid;
 import space.NASADataProvider;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
-import java.net.URL;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -31,9 +24,11 @@ public class MainController {
     @FXML
     private TextField endDateInput;
     @FXML
-    private Button mainButton;
+    private TextField user;
     @FXML
-    private Button logInButton;
+    private TextField password;
+    @FXML
+    Label error;
 
     @FXML
     private void getData() {
@@ -50,16 +45,10 @@ public class MainController {
                 endDateInput.setText("");
                 startDateInput.setPromptText("e.g. 2000-12-31");
                 endDateInput.setPromptText("e.g. 2000-12-31");
-////                listView.getItems().clear();
-////                listView.getItems().add("An error has occurred.\nPlease enter both dates in the following format: YYYY-MM-DD.");
             } else if (!isValidDate(startDateInput.getText())) {
-////                listView.getItems().clear();
-////                listView.getItems().add("The start date is invalid.\n Please enter a date in the format: YYYY-MM-DD.");
                 startDateInput.setText("");
                 startDateInput.setPromptText("e.g. 2000-12-31");
             } else if (!isValidDate(endDateInput.getText())) {
-////                listView.getItems().clear();
-////                listView.getItems().add("The end date is invalid.\nPlease enter the date in the following format: YYYY-MM-DD.");
                 endDateInput.setText("");
                 endDateInput.setPromptText("e.g. 2000-12-31");
             }
@@ -72,21 +61,6 @@ public class MainController {
         diameter.setCellValueFactory(new PropertyValueFactory<Asteroid, Float>("diameter"));
         hazardous.setCellValueFactory(new PropertyValueFactory<Asteroid, Boolean>("isHazardous"));
         tableView.setItems(data);
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-//        if (asteroids != null) {
-//            listView.getItems().clear();
-//            for (Asteroid asteroid : asteroids) {
-//                /* textArea.appendText() */
-//                listView.getItems().add(asteroid.getDate().format(formatter) +
-//                        " - " + (String.valueOf(Math.abs(Math.round(asteroid.getDistance() / 1_000_00f) / 10f)).length() < 4 ?
-//                        "  " + Math.round(asteroid.getDistance() / 1_000_00f) / 10f + "mln km from earth, " :
-//                        Math.round(asteroid.getDistance() / 1_000_00f) / 10f + "mln km from earth, ") +
-//                        " " + (String.valueOf(Math.abs(Math.round(asteroid.getDiameter() * 100f) / 100f)).length() < 4 ?
-//                        "  " + Math.round(asteroid.getDiameter() * 100f) / 100f + "km diameter, " :
-//                        Math.round(asteroid.getDiameter() * 100f) / 100f + "km diameter, ") +
-//                        " " + (asteroid.getHazardous() ? "it is hazardous!" : "it's not hazardous!") + "\n");
-//            }
-//        }
     }
 
     private boolean isValidDate(String dateInput) {
@@ -95,7 +69,10 @@ public class MainController {
 
     @FXML
     private void getSecondWindow() throws IOException {
-        GUIApplication.setRoot("/main");
+        if (user.getText().equals("user") && password.getText().equals("password"))
+            GUIApplication.setRoot("/main");
+        else
+            error.setText("Wrong username or password!");
     }
 
 }
